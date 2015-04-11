9b735f9a073b1ed7b8137c8ca5b33cb85e38425c
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-04-11 12:23:27 +0100

    First commit for libRoadRunner simulator

---
+++ src/main/java/org/geppetto/simulator/libroadrunner/LibRoadRunnerSimulatorService.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2011 - 2015 OpenWorm.
+ * http://openworm.org
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+package org.geppetto.simulator.libroadrunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geppetto.core.beans.SimulatorConfig;
+import org.geppetto.core.common.GeppettoExecutionException;
+import org.geppetto.core.common.GeppettoInitializationException;
+import org.geppetto.core.model.IModel;
+import org.geppetto.core.model.runtime.AspectNode;
+import org.geppetto.core.services.IModelFormat;
+import org.geppetto.core.services.registry.ServicesRegistry;
+import org.geppetto.core.simulation.IRunConfiguration;
+import org.geppetto.core.simulation.ISimulatorCallbackListener;
+import org.geppetto.core.simulator.ASimulator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * @author kylemedley
+ * @author matteocantarelli
+ * 
+ */
+@Service
+public class LibRoadRunnerSimulatorService extends ASimulator
+{
+
+	@Autowired
+	private SimulatorConfig libRoadRunnerSimulatorConfig;
+
+	private static Log _logger = LogFactory.getLog(LibRoadRunnerSimulatorService.class);
+
+	@Override
+	public void initialize(List<IModel> models, ISimulatorCallbackListener listener) throws GeppettoInitializationException, GeppettoExecutionException
+	{
+		super.initialize(models, listener);
+	}
+
+	@Override
+	public void simulate(IRunConfiguration arg0, AspectNode aspect) throws GeppettoExecutionException
+	{
+
+	}
+
+
+	@Override
+	public String getName()
+	{
+		return this.libRoadRunnerSimulatorConfig.getSimulatorName();
+	}
+
+	@Override
+	public String getId()
+	{
+		return "libRoadRunnerSimulator";
+	}
+
+	@Override
+	public void registerGeppettoService()
+	{
+		List<IModelFormat> modelFormatList = new ArrayList<IModelFormat>();
+		modelFormatList.add(ModelFormat.SBML);
+		ServicesRegistry.registerSimulatorService(this, modelFormatList);
+	}
+
+}
---
+++ src/main/java/org/geppetto/simulator/libroadrunner/LibRoadRunnerUpdateSimulationTreeVisitor.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2011 - 2015 OpenWorm.
+ * http://openworm.org
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *     	OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+package org.geppetto.simulator.libroadrunner;
+
+import org.geppetto.core.model.runtime.AspectNode;
+import org.geppetto.core.model.runtime.AspectSubTreeNode;
+import org.geppetto.core.model.runtime.AspectSubTreeNode.AspectTreeType;
+import org.geppetto.core.model.runtime.EntityNode;
+import org.geppetto.core.model.runtime.VariableNode;
+import org.geppetto.core.model.state.visitors.DefaultStateVisitor;
+
+/**
+ * @author kylemedley
+ * @author matteocantarelli
+ * 
+ */
+public class LibRoadRunnerUpdateSimulationTreeVisitor extends DefaultStateVisitor
+{
+
+	private AspectNode _aspect;
+	private boolean _modifiedSimulationTree = false;
+
+	public LibRoadRunnerUpdateSimulationTreeVisitor(AspectNode aspect)
+	{
+		_aspect = aspect;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.geppetto.core.model.state.visitors.DefaultStateVisitor#inAspectNode (org.geppetto.core.model.runtime.AspectNode)
+	 */
+	@Override
+	public boolean inAspectNode(AspectNode node)
+	{
+		// we only visit the nodes which belong to the same aspect
+		if(node.getId().equals(_aspect.getId()))
+		{
+			return super.inAspectNode(node);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.geppetto.core.model.state.visitors.DefaultStateVisitor# outAspectSubTreeNode(org.geppetto.core.model.runtime.AspectSubTreeNode)
+	 */
+	@Override
+	public boolean outAspectSubTreeNode(AspectSubTreeNode node)
+	{
+		if(node.getType().equals(AspectTreeType.WATCH_TREE) && _modifiedSimulationTree)
+		{
+			node.setModified(true);
+			_modifiedSimulationTree = false;
+			AspectNode aspectNode = (AspectNode) node.getParent();
+			aspectNode.setModified(true);
+			((EntityNode) aspectNode.getParentEntity()).updateParentEntitiesFlags(true);
+		}
+		return super.outAspectSubTreeNode(node);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.geppetto.core.model.state.visitors.DefaultStateVisitor#visitVariableNode (org.geppetto.core.model.runtime.VariableNode)
+	 */
+	@Override
+	public boolean visitVariableNode(VariableNode node)
+	{
+
+		return super.visitVariableNode(node);
+	}
+
+
+}
